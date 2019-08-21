a02c9ab4b439b06b57b62fdbf2421f66ba309cd2
Yago Lopes <yagolopeslazaro@gmail.com>
2019-08-21 16:02:47 -0300

    add config reactotron

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import { Provider } from "react-redux"; //O Provider passará o stado do redux para todos os componentes que estão dentro dele
 import store from "./store";
 import TodoList from "./TodoList";
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}> 
-        <div className="App">
-          <TodoList />
-        </div>
-      </Provider>
-    );
-  }
-}
+import "./config/ReactotronConfig";
+
+console.tron.log("testando");
+
+const App = () => (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
 
 export default App;
