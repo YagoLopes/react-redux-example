6130503fa580b7105e70405d5d184ecd8f79952d
Yago Lopes <yagolopeslazaro@gmail.com>
2019-08-21 21:10:19 -0300

    resolvendo erros de config do reactotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./config/ReactotronConfig";
 import { Provider } from "react-redux"; //O Provider passará o stado do redux para todos os componentes que estão dentro dele
 import store from "./store";
 import TodoList from "./TodoList";
 
-import "./config/ReactotronConfig";
+
 
 console.tron.log("testando");
 
